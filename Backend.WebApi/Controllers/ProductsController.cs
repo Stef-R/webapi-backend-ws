3dda93495a2bbdfd3541fc1298487866b86df28b
Stef-R <username@user.noreply.github.com>
2016-12-06 11:38:43 +0100

    add model og control scraping builder

---
+++ Backend.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Backend.WebApi.Models;
+
+
+namespace Backend.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products;
+
+        public ProductsController() 
+        {
+            products = new Product[]
+            {
+                new Product() { Id=1, Name="Pizza", Category= "Mad", Price=(decimal) 49.50 },
+                new Product() { Id=2, Name="Cola", Category= "Drik", Price=(decimal) 19.50 },
+                new Product() { Id=3, Name="Øl", Category= "Drik", Price=(decimal) 24.50 },
+                new Product() { Id=4, Name="Is", Category= "Slik", Price=(decimal) 14.50 },
+            };
+
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        public IHttpActionResult GetProducts(int id)
+        {
+            foreach (var item in products)
+            {
+                if (item.Id == id)
+                {
+                    return Ok(item);
+                }
+            }      
+            return NotFound();            
+        }
+    }
+}
---
+++ Backend.WebApi/Models/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.WebApi.Models
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private string category;
+        private decimal price;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
