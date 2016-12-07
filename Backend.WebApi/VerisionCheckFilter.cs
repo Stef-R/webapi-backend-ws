e0ed672f0f9851cb4c10d1346b02b99446ae5989
Stef-R <username@user.noreply.github.com>
2016-12-07 14:57:13 +0100

    add action filter: VerisionCheckFilter

---
+++ Backend.WebApi/VerisionCheckFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Backend.WebApi
+{
+    public class VerisionCheckFilter : IActionFilter
+    {
+        public bool AllowMultiple
+        {
+            get
+            {
+                return false;
+                //throw new NotImplementedException();
+            }
+        }
+
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(
+            HttpActionContext actionContext, 
+            CancellationToken cancellationToken, 
+            Func<Task<HttpResponseMessage>> continuation)
+        {
+            // do
+            HttpResponseMessage response = await continuation();
+
+            //do 
+
+            return response;
+
+
+            //throw new NotImplementedException();
+        }
+    }
+}
